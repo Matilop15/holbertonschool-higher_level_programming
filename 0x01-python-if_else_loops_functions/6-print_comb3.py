43f99d2f89edfd0c8d140a90a1c2c39fadfe8ac5
Matias <3959@holbertonschool.com>
2021-12-21 10:41:45 -0800

    task 6 fix betty

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for a in range(10):
     for b in range(10):
         if ((a != b and a < b) and a < 9):
             if (a == 8 and b == 9):
-                print('{0}{1}'.format(a,b))
+                print('{0}{1}'.format(a, b))
             else:
-                print('{0}{1}, '.format(a,b), end='')
+                print('{0}{1}, '.format(a, b), end='')
