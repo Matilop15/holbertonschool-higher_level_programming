ba37d25cb86ca6f738716d5295c9bbd76edc01c4
Matias <3959@holbertonschool.com>
2021-12-21 07:22:50 -0800

    fix task 5 part1, delete prints

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 for i in range(00, 100):
-    if (i < 10):
-        print('0{:d}'.format(i), end='')
-
-    else:
-        print('{:d}'.format(i), end='')
-
     if (i < 99):
-        print(', ', end='')
-    if (i == 99):
-        print()
+        print('{:02d}, '.format(i), end='')
+    else:
+        print(i)
