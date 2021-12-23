97c545f5cbcaabb5dad25f0eb0d7465d4feefab5
Matias <3959@holbertonschool.com>
2021-12-23 08:38:20 -0800

    fix output 4, 6, 8 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-
+import sys
 if __name__ == "__main__":
-    from sys import argv
-    argum = argv
-    arc = len(argum) - 1
-    if arc == 0:
+    arc = len(sys.argv)
+    if arc == 1:
         print("0 arguments.")
-    elif arc == 1:
-        print("1 argument:\n1: {}".format(argum[1]))
-    elif arc > 1:
+    elif arc == 2:
+        print("1 argument:\n1: {:s}".format(sys.argv[1]))
+    else:
         print("{:d} argments:".format(arc))
-        for a in range(1, arc + 1):
-            print("{:d}: {:s}".format(a, argum[a]))
+        for a in range(1, arc):
+            print("{:d}: {:s}".format(a, sys.argv[a]))
