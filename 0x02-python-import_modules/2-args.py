9607d87106683ba24b58f3d27bd17cf678cd923e
Matias <3959@holbertonschool.com>
2021-12-23 08:54:59 -0800

    fix argument mal escrito

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 import sys
 if __name__ == "__main__":
     argc = len(sys.argv)
-    if argc == 1:
-        a = 'arguments.'
-    elif argc == 2:
-        a = 'argument:'
-    else:
-        a = 'arguments:'
-    print("{:d} {:s}".format(argc - 1, a))
-    for i in range(1, argc - 1):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+	if arc == 1:
+		print("0 arguments.")
+	elif arc == 2:
+		print("1 argument:")
+		print("{:d}: {:s}".format(arc - 1, sys.argv[1]))
+	else:
+		print("{:d} arguments:".format(arc - 1))
+	for a in range(1, arc):
+		print("{:d}: {:s}".format(a, sys.argv[a]))
