87fd2748583efaed17ffe7e3e491e78c48ea5a96
Matias <3959@holbertonschool.com>
2021-12-23 07:45:11 -0800

    add task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argum = sys.argv
+    arc = len(argum) - 1
+    if arc == 0:
+        print("0 arguments.")
+    elif arc == 1:
+        print("1 argument:\n1: {}".format(argum[1]))
+    elif arc > 0:
+            print("{:d} argments:".format(arc))
+            for a in range(1, arc + 1):
+                print("{}: {}".format(a, argum[a]))
