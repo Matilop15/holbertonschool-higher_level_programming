7bcf29115241077ee975eec9f0c0a4b722f1896c
Matias <3959@holbertonschool.com>
2021-12-23 08:49:38 -0800

    asdas

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    largo = len(sys.argv)
-    if largo == 1:
+    argc = len(sys.argv)
+    if argc == 1:
         a = 'arguments.'
-    elif largo == 2:
+    elif argc == 2:
         a = 'argument:'
     else:
         a = 'arguments:'
-    largo2 = largo - 1
-    print("{:d} {:s}".format(largo2, a))
-    for i in range(1, largo):
+    print("{:d} {:s}".format(argc - 1, a))
+    for i in range(1, argc - 1):
         print("{:d}: {:s}".format(i, sys.argv[i]))
