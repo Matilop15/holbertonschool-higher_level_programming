0eec562eb348b8b41df21c3ffdde56e1667dfb7e
Matias <3959@holbertonschool.com>
2022-01-03 18:03:55 -0800

    task 100

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "Python.h"
+/**
+ * print_python_list_info - Prints info Python lists.
+ * @p: A PyObject list.
+ */
+void print_python_list_info(PyObject *p)
+{
+	PyObject *obj;
+
+	printf(obj)
+}
