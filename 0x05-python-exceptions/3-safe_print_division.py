abecd825c780bc9ac195a078f2e2650fab7a8bb6
Matias <3959@holbertonschool.com>
2022-01-17 06:27:05 -0800

    add task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    res = 0
+    try:
+       res = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        if res == 0:
+            res = None
+            print("Inside result: {}".format(res))
+            return None
+        else:
+            print("Inside result: {}".format(res))
+            return res
