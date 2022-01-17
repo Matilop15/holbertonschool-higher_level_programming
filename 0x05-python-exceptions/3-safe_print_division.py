1ab8fedd655ce44b61514e5e29a3a4288a5459bd
Matias <3959@holbertonschool.com>
2022-01-17 08:11:11 -0800

    fix check error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    res = 0
     try:
         res = a / b
     except ZeroDivisionError:
-        return None
+        res = None
     finally:
-        if res == 0:
-            res = None
-            print("Inside result: {}".format(res))
-            return None
-        else:
             print("Inside result: {}".format(res))
             return res
