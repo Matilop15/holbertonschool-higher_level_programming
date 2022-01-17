66871f44916b12306faefecf5ed365ec2bd292eb
Matias <3959@holbertonschool.com>
2022-01-17 09:50:01 -0800

    fix pecocho

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+
 from sys import stderr
+
 
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except (TypeError, ValueError) as exept:
-        print("Exeption: {}".format(exept), file = stderr)
+        print("Exeption: {}".format(exept), file=stderr)
         return False
