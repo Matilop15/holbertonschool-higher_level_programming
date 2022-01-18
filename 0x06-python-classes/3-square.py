9134fcd57cdeea4a0e1e83537a12a897eef4a9b5
Matias <3959@holbertonschool.com>
2022-01-18 08:41:48 -0800

    add comment

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+ a class Square that defines a square by: based on 2-square.py
+"""
+
 
 class Square:
+    """
+    Private instance attribute: size
+    Instantiation with optional size: def __init__(self, size=0):
+    Public instance method: def area(self):
+    that returns the current square area
+    """
     def __init__(self, size=0):
         self.__size = size
         if type(self.__size) is not int:
