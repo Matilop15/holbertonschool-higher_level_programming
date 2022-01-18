17a4a198b8dd724ba52e97dd7eae536e62a27e46
Matias <3959@holbertonschool.com>
2022-01-18 11:14:13 -0800

    fix comment

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+class Square:
+    """Square Class
+    """
+
+    def __init__(self, size=0):
+        """
+        The __init__ method initializes the size value of the square.
+        Attributes:
+            size (:obj:`int`, optional): The size of the square.
+        Raises:
+            TypeError: If `size` type is not `int`.
+            ValueError: If `size` is less than `0`.
+     """
+
+        self.__size = size
+
+        if type(self.__size) is not int:
+            raise TypeError('size must be an integer')
+
+        if self.__size < 0:
+            raise ValueError('size must be >= 0')
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+
+        self.__size = size
+
+        if type(self.__size) is not int:
+            raise TypeError('size must be an integer')
+
+        if self.__size < 0:
+            raise ValueError('size must be >= 0')
+
+        def area(self):
+            """
+            Returns square area
+            """
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size > 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
+        else:
+            print("")
