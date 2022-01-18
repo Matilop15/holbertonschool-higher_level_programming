0c540a046c99976e339744114a1b13763e24dabe
Matias <3959@holbertonschool.com>
2022-01-18 10:59:48 -0800

    erorrrrrrrrrrrrr

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
-Module 0-square
+Module 0-scuare.py
 Define a empty Class Square
 """
 
 
 class Square():
-    """Empty Class"""
+    """
+    Empty Class
+    """
     pass
