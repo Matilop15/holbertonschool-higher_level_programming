d275bfcbb4878ce010556eb0084619a3616c2bb8
Matias <3959@holbertonschool.com>
2022-01-18 10:57:32 -0800

    fix pecocho

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,10 +13,9 @@ class Square:
             TypeError: If `size` type is not `int`.
             ValueError: If `size` is less than `0`.
      """
-        
 
         self.__size = size
-    
+
         if type(self.__size) is not int:
             raise TypeError('size must be an integer')
 
@@ -29,15 +28,15 @@ class Square:
 
     @size.setter
     def size(self, size):
-        
+
         self.__size = size
-        
+
         if type(self.__size) is not int:
             raise TypeError('size must be an integer')
 
         if self.__size < 0:
             raise ValueError('size must be >= 0')
-    
+
     def area(self):
         """Returns square area
         """
