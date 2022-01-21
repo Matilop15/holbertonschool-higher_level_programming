1ec7c4912da27c00c6ecc05c7c2fb981b4a7f8ad
Matias <3959@holbertonschool.com>
2022-01-21 11:42:17 -0800

    changee coment

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """
+
 0-add_integer.py
 Function that adds 2 integers.
 """
 
 
 def add_integer(a, b=98):
-    """
-    Comprobar que el numbero no es un float,
-    si lo es convertirlo a int.
-    si no es un float ni un int devolver
-    typeError(mensage): a/b must be an integer
-    Return a + b
-    """
+    """Comprobar que el numbero no es un float,
+    si no es un float ni un int devolver: a/b must be an integer
+    Return a + b"""
     if type(a) not in (int, float):
         raise TypeError('a must be an integer')
     if type(b) not in (int, float):
