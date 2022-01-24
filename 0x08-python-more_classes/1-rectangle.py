07773ab8b9e17778f1a9b67f026c0d5fdc59f95f
Matias <3959@holbertonschool.com>
2022-01-24 14:14:31 -0800

    fix tas k 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,28 +10,13 @@ class Rectangle:
     aca van los comentarios
     """
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
-
-    @property
-    def height(self):
-        ''' property of heigh'''
-        return self.__height
+        self.height = height
 
     @property
     def width(self):
         ''' property of widh'''
         return self.__width
-
-    @height.setter
-    def height(self, value):
-        ''' height setter '''
-        if not(isinstance(value, int)):
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
 
     @width.setter
     def width(self, value):
@@ -42,3 +27,18 @@ class Rectangle:
             raise TypeError("width must be >= 0")
         else:
             self.__width = value
+	
+    @property
+    def height(self):
+        ''' property of heigh'''
+        return self.__height
+	
+    @height.setter
+    def height(self, value):
+        ''' height setter '''
+        if not(isinstance(value, int)):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
