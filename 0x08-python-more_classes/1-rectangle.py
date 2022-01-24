4bf0143deac79138e4dd2107eb45569eb2865d3e
Matias <3959@holbertonschool.com>
2022-01-24 14:22:27 -0800

    pycodstyel

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
-    
+
     @property
     def width(self):
         ''' property of width'''
@@ -27,12 +27,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
-	
+
     @property
     def height(self):
         ''' property of heighit'''
         return self.__height
-	
+
     @height.setter
     def height(self, value):
         ''' height setter '''
