0e8a9c3d4d4c8b0adf5cd9041a4754b739238896
Matias <3959@holbertonschool.com>
2022-01-31 10:52:26 -0800

    add coments

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -13,6 +13,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """calculate size and validate with integer validator"""
     def __init__(self, size):
+        """validar si es un integer y retornar area"""
         self.__size = size
         self.integer_validator("size", size)
-        super().__init__(size, size)
+        super().__init__(size, size) """ super sirve para modificar en la clase padre"""
