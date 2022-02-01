dc907a3c4d1cd0e49757ef2358e87c97b59e2c85
Matias <3959@holbertonschool.com>
2022-02-01 11:51:54 -0800

    add task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Matias López <3959@holbertonschool.com>
+"""
+7-add_item.py
+a script that adds all arguments to a Python list
+"""
+
+
+import sys
+
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+open("add_item.json", "a")
+try:
+    l = load_from_json_file("add_item.json")
+except ValueError:
+    l = []
+save_to_json_file(l + sys.argv[1:], "add_item.json")
