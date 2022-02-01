feae47b708b6a89f050b35196823b97ddf9740d2
Matias <3959@holbertonschool.com>
2022-02-01 12:01:09 -0800

    fix error output task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,14 +7,13 @@ a script that adds all arguments to a Python list
 
 
 import sys
+import json
 
-
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-open("add_item.json", "a")
 try:
-    l = load_from_json_file("add_item.json")
-except ValueError:
-    l = []
-save_to_json_file(l + sys.argv[1:], "add_item.json")
+    arch = load_from_json_file("add_item.json")
+    save_to_json_file(arch + sys.argv[1:], "add_item.json")
+except Exception:
+    save_to_json_file(sys.argv[1:], "add_item.json")
