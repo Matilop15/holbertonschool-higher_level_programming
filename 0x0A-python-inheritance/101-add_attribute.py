742917002fa5634aec41cad9112257f49ba0e3a3
Matias <3959@holbertonschool.com>
2022-01-31 17:51:33 -0800

    add task 101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Matias Lòpez <3959@holbertonschool.com>
+"""
+a function that adds a new attribute to an object if its possible
+"""
+
+
+def add_attribute(obj, att, value):
+    """add_attribure: add a new objt
+    raise typerror if attribute not added
+    """
+    if type(obj) is int:
+        raise TypeError("can`t add new attribute")
+
+    return att, value
