65f7dd14d4804a1829e59cbbe088da9c318b7af6
Matias <3959@holbertonschool.com>
2022-02-01 08:48:56 -0800

    fix indent error

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -8,6 +8,7 @@ a function that returns the JSON representation of an object (string)
 
 import json
 
+
 def to_json_string(my_obj):
     """change representacion of an object with JSON"""
     return json.dumps(my_obj)
