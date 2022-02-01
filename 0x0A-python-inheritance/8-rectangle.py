63c379e4dc57667b306e8a0956e9e11e34f0c8c1
Matias <3959@holbertonschool.com>
2022-01-31 16:40:28 -0800

    import task 7

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,31 +6,18 @@ add width and heigth
 """
 
 
-class BaseGeometry:
-    """
-    add integer validator to anterior task
-    """
-    def area(self):
-        """raise Exception"""
-        raise Exception("area() is not implemented")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    def integer_validator(self, name, value):
-        """check TypeError and ValueError"""
-        if type(value) is not int:
-            raise TypeError(name + " must be an integer")
-        if value <= 0:
-            raise ValueError(name + " must be greater than 0")
 
-    class Rectangle(BaseGeometry):
-        """create subclass Rectanble of BaseGeometry"""
+class Rectangle(BaseGeometry):
+    """create subclass Rectanble of BaseGeometry"""
+    def __init__(self, width, height):
+        """
+        width and height must be privat
+        and must be positive integer
+        """
+        self.__width = width
+        self.__height = height
 
-        def __init__(self, width, height):
-            """
-            width and height must be privat
-            and must be positive integer
-            """
-            self.__width = width
-            self.__height = height
-
-            self.integer_validator("width", width)
-            self.integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
