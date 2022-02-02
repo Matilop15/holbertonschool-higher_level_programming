3cbd41438c1cfe6db5e05f4b33a4f770837e7290
Matias <3959@holbertonschool.com>
2022-02-01 18:08:00 -0800

    fix error output 2

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,8 +12,12 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+arglist = list(sys.argv[1:])
+
 try:
-    arch = load_from_json_file("add_item.json")
-    save_to_json_file(arch + sys.argv[1:], "add_item.json")
+    old_data = load_from_json_file('add_item.json')
 except Exception:
-    save_to_json_file(sys.argv[1:], "add_item.json")
+    old_data = []
+    
+    old_data.extend(arglist)
+    save_to_json_file(old_data, 'add_item.json')
