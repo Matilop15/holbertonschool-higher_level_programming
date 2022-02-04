e27476955e1b8a84550c90d00c507bc6b7a15d76
Matias <3959@holbertonschool.com>
2022-02-04 04:14:55 -0800

    add test base

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""
+Test for base.py
+"""
+
+import unittest
+
+from models.base import Base
+
+
+class test_case(unittest.TestCase):
+    """holberton"""
+    
+    def test_base(self):
+        """
+        check if base id no args
+        """
+        b1 = Base(None)
+        self.assertEqual(b1.id, 1)
+
+    def test_baseNone(self):
+        """
+        check if base id is None
+        """
+        self.assertEqual(Base().id, 1)
+
+    def test_base_12(self):
+        """
+        check if base id with 1 arg
+        """
+        b1 = Base(12)
+        self.assertEqual(b1.id, 12)
+
+    def test_base_args(self):
+        """
+        check if base id with 2 agrs
+        """
+        self.assertRaises(TypeError, Base(12, 4))
+
+    def test_id_inc(self):
+        """
+        check if base id increment
+        """
+        b1 = Base()
+        b1 = Base()
+        self.assertEqual(b1.id, 2)
+
+    def test_id_inc2(self):
+        """
+        check if base id increment
+        """
+        b1 = Base()
+        b2 = Base(12)
+        b3 = Base()
+        self.assertEqual(b3.id, 2)
+
+    def test_id_neg(self):
+        """
+        check base id negaative
+        """
+        b1 = Base(-1)
+        self.assertEqual(b1.id, -1)
+
+    def test_id_str(self):
+        """
+        check base id str
+        """
+        self.assertRaises(TypeError, Base, "hola")
+
+    def test_id_float(self):
+        """
+        check base id float
+        """
+        self.assertRaises(TypeError, Base, 1.0)
+
+    def test_id_bool(self):
+        """
+        check base id bool
+        """
+        self.assertRaises(TypeError, Base, True)
