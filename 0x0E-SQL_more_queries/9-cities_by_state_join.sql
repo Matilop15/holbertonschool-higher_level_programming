b1c7a814d4b2f9a8295fc23e82aa2697979fefb7
Matias <3959@holbertonschool.com>
2022-03-09 10:49:08 -0800

    fix code

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- list all cities contained in the database "hbtn"
 SELECT cities.id, cities.name, states.name 
-FROM cities JOIN states ON cities.states_id = states.id
+FROM cities JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
