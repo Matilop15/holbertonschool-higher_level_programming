b0c46d13b3ef191b4a789d20497abd13db7894c6
Matias <3959@holbertonschool.com>
2022-03-09 10:15:04 -0800

    fix code

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- list all cities of california in the database "hbtn"
-SELECT id, name FROM cities;
+SELECT id, name FROM cities
 WHERE state_id = (
 	SELECT id FROM cities WHERE name = "California";
 );
