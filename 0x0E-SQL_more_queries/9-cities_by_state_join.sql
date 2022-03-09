b8c5686d6de4dd8498314d511f710e4700c6f171
Matias <3959@holbertonschool.com>
2022-03-09 10:43:41 -0800

    add task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- list all cities contained in the database "hbtn"
-SELECT cities.id, cities.name, state.name FROM cities
-RIGHT JOIN states ON states.id = cities.states_id
-ORDER BY city.id
+SELECT cities.id, cities.name, state.name 
+FROM cities JOIN states ON states.id = cities.states_id
+ORDER BY cities.id ASC;
