594048d2c81685958001c5b893b77786a87b3bfd
Matias <3959@holbertonschool.com>
2022-03-09 09:59:39 -0800

    fix code output error

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,10 @@
 -- create the database "hbtn" and the table cities
+-- with FOREIGN KEY
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES state(id)
+	FOREIGN KEY (state_id) REFERENCES state(id)
 );
