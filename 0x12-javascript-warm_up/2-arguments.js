f77e7085d73b0564ebe28d546e591276bf8bf4d8
Matias <3959@holbertonschool.com>
2022-04-04 08:14:42 -0700

    add comments task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
+
+// If no arguments are passed to the script, print “No argument”
+// If only one argument is passed to the script, print “Argument found”
+
 const len = process.argv.length;
 if (len === 2) {
   console.log('No argument');
