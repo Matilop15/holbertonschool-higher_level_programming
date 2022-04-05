4baa0cf2e01702adaa3a0ccec7926209ea5361d1
Matias <3959@holbertonschool.com>
2022-04-05 09:13:01 -0700

    add task 5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,48 @@
+#!/usr/bin/node
+
+// Write a class Rectangle that defines a rectangle
+// f w or h is equal to 0 or not a positive integer, create an empty object
+// update task 3: print rectangle and mmultipli values of widht and height
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w <= 0 || h <= 0) || !w || !h) {
+      return NaN;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let row = '';
+    for (let count = 0; count < this.width; count++) {
+      row += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+
+  double () {
+    const newWidth = this.width * 2;
+    const newHeight = this.height * 2;
+    this.width = newWidth;
+    this.height = newHeight;
+  }
+
+  rotate () {
+    const rotWidth = this.height;
+    const rotHeight = this.width;
+    this.width = rotWidth;
+    this.height = rotHeight;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
