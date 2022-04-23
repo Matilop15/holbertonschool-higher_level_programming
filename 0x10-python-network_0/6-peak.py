3491ff6185ebda91c5007bc1570667c70c3bdc59
Matias <3959@holbertonschool.com>
2022-04-22 17:58:32 -0700

    ix code, add coments

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,6 +5,7 @@ Finds a peak
 
 
 def find_peak(list_of_integers):
+    """Find peak in unsorted list"""
     if len(list_of_integers) == 0:
         return None
     elif len(list_of_integers) == 1:
