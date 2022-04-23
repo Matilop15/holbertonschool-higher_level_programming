f6d1e4ef006836a27db04f3bffd2878c700de1d9
Matias <3959@holbertonschool.com>
2022-04-22 17:31:10 -0700

    fix pep8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,8 @@
 """
 Finds a peak
 """
+
+
 def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return None
@@ -10,13 +12,13 @@ def find_peak(list_of_integers):
     elif len(list_of_integers) == 2:
         a = list_of_integers[0]
         b = list_of_integers[1]
-        return a if a>b else b
+        return a if a > b else b
     else:
-        for l in range(1, len(list_of_integers)-1):
-            a = list_of_integers[l-1]
-            b = list_of_integers[l]
-            c = list_of_integers[l+1]
-            if b>=a and b>=c:
+        for i in range(1, len(list_of_integers)-1):
+            a = list_of_integers[i - 1]
+            b = list_of_integers[i]
+            c = list_of_integers[i + 1]
+            if b >= a and b >= c:
                 peak = b
         if(peak):
             return peak
