39ce476c4d919823562efddae5166c53efe1ce8d
Matias <3959@holbertonschool.com>
2022-04-24 11:07:39 -0700

    fix code

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,8 +6,8 @@ Python script that fetches https://intranet.hbtn.io/status
 import request
 
 if __name__ == "__main__":
-	resp = request.get('https://intranet.hbtn.io/status')
-	if resp is not None:
-	print('Body response:$')
-	print('\t- type: {}'.format(type(resp.txt)))
-	print('\t- content: {}'.format(respt.txt))
+    resp = request.get('https://intranet.hbtn.io/status')
+    if resp is not None:
+        print('Body response:$')
+        print('\t- type: {}'.format(type(resp.txt)))
+        print('\t- content: {}'.format(respt.txt))
