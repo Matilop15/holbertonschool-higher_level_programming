5600109b15fa00aa16eb4de3ae395569e93c98fb
Matilop15 <3959@holbertonschool.com>
2022-04-24 18:04:57 +0000

    add tasj 44

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
+
+import request
+
+if __name__ == "__main__":
+	resp = request.get('https://intranet.hbtn.io/status')
+	if resp is not None:
+	print('Body response:$')
+	print('\t- type: {}'.format(type(resp.txt)))
+	print('\t- content: {}'.format(respt.txt))
