34ccb18baee88ba5389db3c49f23f2895702d5c3
Matias <3959@holbertonschool.com>
2022-04-24 11:44:07 -0700

    fixxx

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,7 +7,7 @@ import requests
 import sys
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     resp = requests.get(sys.argv[1])
     if resp is not None:
         print(resp.header.get('X-Request-Id'))
