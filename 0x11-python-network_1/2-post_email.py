40878f7873b1fbaf73e8b2748bb360105a143c65
Matias <3959@holbertonschool.com>
2022-04-24 10:38:06 -0700

    fix pep 8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,16 +7,14 @@ import sys
 
 
 if __name__ == "__main__":
-	data = urllib.parse.urlencode({'email':sys.argv[2]})
-	data = data.encode('ascii')
-	reqq = urllib.request.Request(sys.argv[1], data)
-	
-	try:
-		with urllib.request.urlopen(reqq) as resp:
-			if resp is not None:
-				html = resp.read()
-				print(html.decode('utf-8'))
-	except Exception:
-		pass
+    data = urllib.parse.urlencode({'email': sys.argv[2]})
+    data = data.encode('ascii')
+    reqq = urllib.request.Request(sys.argv[1], data)
 
-
+    try:
+        with urllib.request.urlopen(reqq) as resp:
+            if resp is not None:
+                html = resp.read()
+                print(html.decode('utf-8'))
+    except Exception:
+        pass
