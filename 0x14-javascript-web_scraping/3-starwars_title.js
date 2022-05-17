3d6a1caf1ad3052ff9960cfd1c66a1bab3690e4d
Matilop15 <3959@holbertonschool.com>
2022-05-17 11:00:42 -0700

    add task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+ * Write a script that prints the title of a Star Wars movie where the episode number matches a given integer.
+ * The first argument is the movie ID
+ */
+
+const axios = require('axios');
+const url = 'https://swapi-api.hbtn.io/api/films/' + (process.argv[2]);
+
+axios.get(url)
+  .then(function (response) {
+	      console.log(response.data.title);
+	    })
+  .catch(function (err) {
+	      console.log(err.response.status);
+	    });
