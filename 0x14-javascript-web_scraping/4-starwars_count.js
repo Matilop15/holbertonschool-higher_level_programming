74d7fb4872dc36f9f625612e116220a94096d64a
Matilop15 <3959@holbertonschool.com>
2022-05-17 13:07:52 -0700

    update all new task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,23 +5,21 @@
 * Wedge Antilles is character ID 18 - your script must use this ID for filtering the result of the API
 */
 
-const axios = require('axios');
-if (!process.argv[2])
-  process.exit();
-
+const axios = require('request');
 const url = process.argv[2];
 let count = 0;
 
-axios.get(url)
-  .then(function (response) {
-    const resp = response.data.results;
-    for (let i = 0; i < resp.length; i++) {
-      if (resp[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-        count++;
+axios(url, function (error, response, body) {
+    const jsoon = JSON.parse(body);
+    const res = jsoon.results;
+    for (let i = 0; i < res.length; i++) {
+      if (res[i].characters) {
+        for (const line of res[i].characters) {
+          if (line.includes('18')) {
+            count++;
+          }
+        }
       }
     }
     console.log(count);
-  })
-  .catch(function (err) {
-    console.log(err.response.status);
-  });
+});
