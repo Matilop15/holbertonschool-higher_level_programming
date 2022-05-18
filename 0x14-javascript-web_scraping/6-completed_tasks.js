c813de8a692788731d1f8e5f51aca38a4a1737c4
Matilop15 <3959@holbertonschool.com>
2022-05-17 17:08:20 -0700

    add task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+/*
+ *Write a script that computes the number of tasks completed by user id.
+ *The first argument is the API URL: https://jsonplaceholder.typicode.com/todos
+ *Only print users with completed task
+ */
+
+const axios = require('axios');
+const url = process.argv[2];
+let cantidad = 0;
+const dict = {};
+
+axios.get(url)
+  .then(function (response) {
+    for (const num in response.data) {
+      const userid = response.data[num].userId;
+      const task = response.data[num].completed;
+      if (dict[userid] >= 1) {
+        cantidad = dict[userid];
+      } else {
+        cantidad = 0;
+      }
+      if (userid && task) {
+        cantidad += 1;
+      }
+      dict[userid] = cantidad;
+    }
+    console.log(dict);
+  });
